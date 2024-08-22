9a32c7d8b66bc7f0bbe30362120acc7ead9cdfc0
sazed1237 <sazedulislam9126@gmail.com>
2024-08-22 14:32:20 +0600

    change connection

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const tradeCollection = client.db("novaMarket").collection("trades");
         const usersCollection = client.db("novaMarket").collection("users");
@@ -318,9 +318,9 @@ async function run() {
         })
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
